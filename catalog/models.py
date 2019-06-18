ae95175ca77a59264ae54bc3ff16217ce7e43e00
Sunil Singh <sunilsingh@Sunils-MacBook-Air.local>
2019-06-18 18:01:39 +0530

    Fix the error on Book model to display the list of book-instances

--- catalog/models.py
+++ catalog/models.py
@@ -30,7 +30,7 @@ class Book(models.Model):
     # Genre class has already been defined so we can specify the object above.
     genre = models.ManyToManyField(Genre, help_text='Select a genre for this book')
     language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
-
+    
     def __str__(self):
         """String for representing the Model object."""
         return self.title
@@ -38,6 +38,15 @@ class Book(models.Model):
     def get_absolute_url(self):
         """Returns the url to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
+
+    
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+    
+    
+    display_genre.short_description = 'Genre'
+
 
 
 class BookInstance(models.Model):
@@ -116,11 +125,7 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'display_genre')
     inlines = [BooksInstanceInline]
 
-    def display_genre(self):
-        """Create a string for the Genre. This is required to display genre in Admin."""
-        return ', '.join(genre.name for genre in self.genre.all()[:3])
 
-        display_genre.short_description = 'Genre'
 
 
 # Register the Admin classes for BookInstance using the decorator
